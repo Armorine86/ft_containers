1711919086afb1e27e2464427856b7ae834ac2e9
Maxime Mondello <mmondell@student.42quebec.com>
2022-04-14 15:56:20 -0400

    Iterator Traits

--- src/Vector/Vec_Iterator.hpp
+++ src/Vector/Vec_Iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: mmondell <mmondell@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 10:16:30 by mmondell          #+#    #+#             */
-/*   Updated: 2022/04/13 11:55:23 by mmondell         ###   ########.fr       */
+/*   Updated: 2022/04/14 15:54:43 by mmondell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,34 @@
 
 #include <cstddef>
 
-namespace ft {
-	
-}
+namespace ft
+{
+
+template<typename Iter>
+struct iterator_traits {
+    typedef typename Iter::iterator_category iterator_category;
+    typedef typename Iter::reference reference;
+    typedef typename Iter::pointer pointer;
+    typedef typename Iter::value_type value_type;
+    typedef typename Iter::difference_type difference_type;
+};
+
+template<typename Iter>
+struct iterator_traits<Iter*> {
+    typedef Iter::iterator_category iterator_category;
+    typedef Iter::reference reference;
+    typedef Iter::pointer pointer;
+    typedef Iter::value_type value_type;
+    typedef Iter::difference_type difference_type;
+};
+
+template<typename Iter>
+struct iterator_traits<const Iter*> {
+    typedef Iter::iterator_category iterator_category;
+    typedef Iter::reference reference;
+    typedef Iter::pointer pointer;
+    typedef Iter::value_type value_type;
+    typedef Iter::difference_type difference_type;
+};
+
+} // namespace ft
--- src/Vector/Vector.hpp
+++ src/Vector/Vector.hpp
@@ -6,7 +6,7 @@
 /*   By: mmondell <mmondell@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 10:16:33 by mmondell          #+#    #+#             */
-/*   Updated: 2022/04/14 14:34:36 by mmondell         ###   ########.fr       */
+/*   Updated: 2022/04/14 15:33:27 by mmondell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,30 @@
 
 #include <memory>
 
-namespace ft {
+namespace ft
+{
 
 template<typename T, typename Alloc = std::allocator<T> >
 class vector
 {
   public:
-    typedef Alloc                                      allocator_type;
-    typedef T                                          value_type;
-    typedef T&                                         reference;
-    typedef T*                                         pointer;
-    typedef const T&                                   const_reference;
-    typedef const T*                                   const_pointer;
-    typedef random_access_iterator<value_type>         iterator;
-    typedef random_access_iterator<const value_type>   const_iterator;
-    typedef ft::reverse_iterator<iterator>             reverse_iterator;
-    typedef ft::reverse_iterator<const_iterator>       const_reverse_iterator;
+
+    // *** Member Types ***
+    typedef Alloc allocator_type;
+    typedef T value_type;
+    typedef T& reference;
+    typedef T* pointer;
+    typedef const T& const_reference;
+    typedef const T* const_pointer;
+    typedef random_access_iterator<value_type> iterator;
+    typedef random_access_iterator<const value_type> const_iterator;
+    typedef ft::reverse_iterator<iterator> reverse_iterator;
+    typedef ft::reverse_iterator<const_iterator> const_reverse_iterator;
     typedef iterator_traits<iterator>::difference_type difference_type;
-    typedef
+    typedef allocator_type::size_type size_type;
+
+    // *** Member Functions ***
+
+    
 };
 } // namespace ft
